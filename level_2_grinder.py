2543157b45ab462c76d599d2933a6729890067a2
Gaurav Shukla <heygauravshukla@gmail.com>
2024-05-09 08:28:04 +0530

    Improved comments and printing statements

--- level_2_grinder.py
+++ level_2_grinder.py
@@ -1,20 +1,21 @@
-# Script for grinding ms rewards at level 2.
 import pyautogui
 import time
 import random
 
 
+# Function to search for a query in Windows search
 def search_windows(query):
-    pyautogui.hotkey("win", "s")
+    pyautogui.hotkey("win", "s")  # Press Windows key + S to open search
     time.sleep(2)  # Wait for the search bar to appear
-    pyautogui.write(query)
-    time.sleep(1)
-    pyautogui.press("enter")
+    pyautogui.write(query)  # Type the query
+    time.sleep(1)  # Wait for the search to start
+    pyautogui.press("enter")  # Press Enter to initiate the search
     time.sleep(8)  # Wait for the search results to appear
     pyautogui.hotkey("ctrl", "w")  # Close the opened tab
     time.sleep(2)  # Wait for the tab to be closed
 
 
+# List of items to search for
 search_items = [
     "Stack Overflow",
     "GitHub",
@@ -48,13 +49,13 @@ search_items = [
     "LeetCode",
 ]
 
-
-# Shuffle the search items in place
+# Shuffle the search items randomly
 random.shuffle(search_items)
 
+# Loop through each item and search for it
 for item in search_items:
     print(f"Searching for: {item}")
     search_windows(item)
 
-
-print(f"Congratulations! You've earned MS rewards.")
+# Message indicating completion
+print("Congratulations! You've earned MS rewards.")
